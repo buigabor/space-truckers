f8de4c9ec4a83fb2c19b60a16f5b1e70dbd238c8
Gabriel Bui <bui.gabor97@gmail.com>
2022-10-27 16:06:21 +0200

    Add logger

---
+++ src/logger.ts
@@ -0,0 +1,27 @@
+import { Logger } from "@babylonjs/core/Misc/logger";
+class ConsoleProxy {
+    constructor() {}
+
+    logInfo(message: string) {
+        Logger.Log(message);
+    }
+
+    logWarning(message: string) {
+        Logger.Warn(message);
+    }
+
+    logError(message: string) {
+        Logger.Error(message);
+    }
+
+    logFatal(message: string) {
+        Logger.Error("FATAL: " + message);
+    }
+
+    flushBuffer() {
+        Logger.ClearLogCache();
+    }
+}
+const theProxy = new ConsoleProxy();
+
+export default theProxy;
