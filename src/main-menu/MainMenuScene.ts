1af550ee31a2d7523288297be5980f94b97c5371
Gabriel Bui <bui.gabor97@gmail.com>
2022-11-01 13:29:04 +0100

    Add visual improvements, animations, transitions to main menu

--- src/main-menu/MainMenuScene.ts
+++ src/main-menu/MainMenuScene.ts
@@ -1,15 +1,15 @@
 import { InjectInspector } from "@/InjectInspector.decorator";
-import {
-    menuBackground,
-    MenuItemOptions,
-    playBtnOptions,
-} from "@/main-menu/menuOptions";
+import { menuBackground, selectionIcon } from "@/main-menu/menuOptions";
 import {
     ArcRotateCamera,
     Engine,
+    EventState,
     HemisphericLight,
     MeshBuilder,
+    Observable,
+    Scalar,
     Scene,
+    setAndStartTimer,
     StandardMaterial,
     Texture,
     Vector3,
@@ -23,8 +23,17 @@ import {
     Rectangle,
     TextBlock,
     TextWrapping,
+    Vector2WithInfo,
 } from "@babylonjs/gui";
 import { StarfieldProceduralTexture } from "@babylonjs/procedural-textures";
+
+export interface MenuItemOptions {
+    name?: string;
+    title?: string;
+    background?: string;
+    color?: string;
+    onInvoked?: (ed: Vector2WithInfo, es: EventState) => void;
+}
 
 @InjectInspector
 class MainMenuScene {
@@ -36,7 +45,27 @@ class MainMenuScene {
 
     private menuGrid!: Grid;
 
+    private selectedItemIdx!: number;
+
+    private selectedItemChanged = new Observable<number>();
+
+    private selectorIcon!: Image;
+
+    private selectorAnimationFrame!: number;
+
     public scene: Scene;
+
+    get selectedItemIndex() {
+        return this.selectedItemIdx || -1;
+    }
+
+    set selectedItemIndex(idx: number) {
+        const itemCount = this.menuGrid.rowCount;
+        // TODO: use this in ImageGallery in pillar-of-babylonjs
+        const newIdx = Scalar.Repeat(idx, itemCount);
+        this.selectedItemIdx = newIdx;
+        this.selectedItemChanged.notifyObservers(newIdx);
+    }
 
     constructor(engine: Engine) {
         let scene = new Scene(engine);
@@ -57,6 +86,67 @@ class MainMenuScene {
         this.setupBackgroundEnvironment();
         this.setupUi();
         this.addMenuItems();
+        this.createSelectorIcon();
+
+        this.selectedItemChanged.add((idx) => {
+            const menuGrid = this.menuGrid;
+            const selectedItem = menuGrid.getChildrenAt(idx, 1);
+            if (selectedItem && !selectedItem[0].isEnabled) {
+                this.selectedItemIndex = idx + 1;
+            }
+            this.selectorIcon.isVisible = true;
+            menuGrid.removeControl(this.selectorIcon);
+            menuGrid.addControl(this.selectorIcon, idx);
+        });
+
+        this.scene.whenReadyAsync().then(() => {
+            this.selectedItemIndex = 0;
+            this.onMenuEnter(1000);
+        });
+    }
+
+    private onMenuEnter(duration?: number) {
+        let fadeIn = 0;
+        const fadeTime = duration || 1500;
+        const timer = setAndStartTimer({
+            timeout: fadeTime,
+            contextObservable: this.scene.onBeforeRenderObservable,
+            onTick: () => {
+                const dT = this.scene.getEngine().getDeltaTime();
+                fadeIn += dT;
+                const currAmt = Scalar.SmoothStep(0, 1, fadeIn / fadeTime);
+                this.menuContainer.alpha = currAmt;
+            },
+            onEnded: () => {
+                this.selectedItemIndex = 0;
+            },
+        });
+        return timer;
+    }
+
+    private onMenuLeave(duration: number, onEndedAction = () => {}) {
+        let fadeOut = 0;
+        const fadeTime = duration || 1500;
+
+        // this.menuContainer.isVisible = true;
+
+        const timer = setAndStartTimer({
+            timeout: fadeTime,
+            contextObservable: this.scene.onBeforeRenderObservable,
+            onTick: () => {
+                const dT = this.scene.getEngine().getDeltaTime();
+                fadeOut += dT;
+                const currAmt = Scalar.SmoothStep(1, 0, fadeOut / fadeTime);
+                this.menuContainer.alpha = currAmt;
+                this.menuGrid.alpha = currAmt;
+            },
+            onEnded: () => {
+                this.menuContainer.alpha = 0;
+                this.menuContainer.isVisible = false;
+                if (onEndedAction) onEndedAction();
+            },
+        });
+        return timer;
     }
 
     private setupUi() {
@@ -68,6 +158,7 @@ class MainMenuScene {
         menuContainer.width = 0.8;
         menuContainer.thickness = 5;
         menuContainer.cornerRadius = 13;
+        menuContainer.alpha = 0;
 
         this.guiMenu.addControl(menuContainer);
         this.menuContainer = menuContainer;
@@ -86,7 +177,7 @@ class MainMenuScene {
 
         const titleText = new TextBlock("title", "Space-Truckers");
         titleText.resizeToFit = true;
-        // TODO: fix in pillar-of-babylonjs
+        // TODO: fix in Grid in pillar-of-babylonjs
         titleText.textWrapping = TextWrapping.Ellipsis;
         titleText.fontSize = "72pt";
         titleText.color = "white";
@@ -100,14 +191,33 @@ class MainMenuScene {
     }
 
     private addMenuItems() {
+        const playBtnOptions: MenuItemOptions = {
+            name: "btPlay",
+            title: "Play",
+            background: "red",
+            color: "white",
+            onInvoked: () => console.log("Play button clicked"),
+        };
+
         const playButton = this.createMenuItem(playBtnOptions);
+
+        const exitBtnOptions: MenuItemOptions = {
+            name: "btExit",
+            title: "Exit",
+            background: "white",
+            color: "black",
+            onInvoked: () => this.onMenuLeave(1000),
+        };
+
+        const exitButton = this.createMenuItem(exitBtnOptions);
         this.menuGrid.addControl(playButton, this.menuGrid.children.length, 1);
+        this.menuGrid.addControl(exitButton, 1, 1);
     }
 
     private createMenuItem(opts: MenuItemOptions) {
         const finalConfig = {
-            name: "",
-            title: "",
+            name: "defaultBtnName",
+            title: "Default Title",
             color: "white",
             background: "green",
             onInvoked: undefined,
@@ -134,6 +244,37 @@ class MainMenuScene {
         });
 
         return btn;
+    }
+
+    private createSelectorIcon() {
+        const selectorIcon = new Image("selectorIcon", selectionIcon);
+        selectorIcon.width = "160px";
+        selectorIcon.height = "60px";
+        selectorIcon.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
+        selectorIcon.shadowOffsetX = 5;
+        selectorIcon.shadowOffsetY = 3;
+        selectorIcon.isVisible = false;
+
+        this.menuGrid.addControl(selectorIcon, 1, 0);
+
+        this.selectorIcon = selectorIcon;
+
+        this.selectorAnimationFrame = 0;
+        this.scene.onBeforeRenderObservable.add(() =>
+            this.selectorIconAnimation()
+        );
+    }
+
+    private selectorIconAnimation() {
+        const animTimeSeconds = Math.PI * 2;
+        const dT = this.scene.getEngine().getDeltaTime() / 1000;
+        this.selectorAnimationFrame = Scalar.Repeat(
+            this.selectorAnimationFrame + dT * 5,
+            animTimeSeconds * 10
+        );
+
+        this.selectorIcon.top =
+            Math.sin(this.selectorAnimationFrame).toFixed(0) + "px";
     }
 
     private setupBackgroundEnvironment() {
--- src/main-menu/menuOptions.ts
+++ src/main-menu/menuOptions.ts
@@ -1,22 +1,7 @@
-import { EventState } from "@babylonjs/core";
-import { Vector2WithInfo } from "@babylonjs/gui";
-
-export interface MenuItemOptions {
-    name?: string;
-    title?: string;
-    background?: string;
-    color?: string;
-    onInvoked?: (ed: Vector2WithInfo, es: EventState) => void;
-}
-
-export const playBtnOptions: MenuItemOptions = {
-    name: "btPlay",
-    title: "Play",
-    background: "red",
-    color: "white",
-    onInvoked: () => console.log("Play button clicked"),
-};
-
 export const menuBackground = `https://raw.githubusercontent.com/jelster/space-truckers/ch4/assets/menuBackground.png?${Number(
+    new Date()
+)}`;
+
+export const selectionIcon = `https://raw.githubusercontent.com/jelster/space-truckers/ch4/assets/ui-selection-icon.PNG?${Number(
     new Date()
 )}`;
